39f05a11db0702679e0bf6c3c0b630934731d2e7
eliastooloee <elitooloee@gmail.com>
2019-12-31 11:23:43 -0800

    working on menu

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,7 +1,19 @@
-class CommandLineInterface
+class CommandLineInterface < ActiveRecord::Base
     
-    def greet
-        puts "Welcome to StarControl, the most comprehensive database of interplanetary arrivals and departures."
+    def run
+        greet
+        
     end
-    
+
+    def greet
+        puts "Welcome to StarLog, the most comprehensive database of interplanetary arrivals and departures."
+    end
+
+    def menu
+        puts ("1.") + "Land on a new planet"
+        puts ("2.") + "See all ships that have landed on a planet"
+        puts ("3.") + "Change data for a planet"
+        puts ("4.") + "Delete a record of your landing on a planet"
+    end
+
 end
